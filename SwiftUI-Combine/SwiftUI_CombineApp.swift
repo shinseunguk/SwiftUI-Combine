fc2ff8cdfa5b52a6b3153b7c3fe85ced7059e428
신승욱 <ukseung.dev@incross.com>
2024-06-12 17:28:06 +0900

    Initial Commit

---
+++ SwiftUI-Combine/SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineApp.swift
+//  SwiftUI-Combine
+//
+//  Created by ukseung.dev on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
